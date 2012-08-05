1cf63c6f35a48a9ec23a40df0906aef4e1f7432d
Georges Duplessy <gduplessy@gmail.com>
2012-08-05 15:24:20 -0400

    Add DeletedAt to List

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
+  acts_as_paranoid
+
   attr_accessible :content, :week, :user_id
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805190346) do
+ActiveRecord::Schema.define(:version => 20120805192120) do
 
   create_table "lists", :force => true do |t|
     t.integer  "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120805190346) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.boolean  "week"
+    t.datetime "deleted_at"
   end
 
   create_table "users", :force => true do |t|
