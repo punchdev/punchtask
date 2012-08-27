5b2e70a7d2fc1c207ad577984e7a13f86ae9b743
Georges Duplessy <gduplessy@gmail.com>
2012-08-27 07:11:35 -0400

    Make stripe work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  attr_accessible :plan_id
   has_many :lists do
 
     def today
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805193112) do
+ActiveRecord::Schema.define(:version => 20120827102810) do
 
   create_table "lists", :force => true do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20120805193112) do
     t.string   "remember_token",     :limit => 128
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
